2da76cf7180ca4ecdce01eb3a0bb8d3d47ae5234
Vultariev <vultariev@gmail.com>
2018-08-10 14:08:42 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname $0`
-gcc src/*.c `sdl2-config --cflags --libs` -I"/Users/15410/.local/Homebrew-brew-5198ad4/include/" -o Doriftu
+gcc src/*.c `sdl2-config --cflags --libs` -Iinclude/ -o Doriftu
