77ac07035e27a2de2acd989d88eb3589842b1647
Buckner <15410@C02PD3U2GFWM.local>
2018-08-10 14:18:48 -0400

    fix some functional stuff

--- src/main.c
+++ src/main.c
@@ -4,4 +4,22 @@
 int main(void)
 {
   SDL_Init(SDL_INIT_VIDEO);
+  SDL_Event event;
+  SDL_Window *win = SDL_CreateWindow("Doriftu", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 400, 0);
+
+  while(1)
+  {
+    while(SDL_PollEvent(&event))
+    {
+      if(event.type == SDL_QUIT)
+      {
+        goto end;
+      }
+    }
+  }
+
+  end:
+  SDL_DestroyWindow(win);
+  SDL_Quit();
+  return 0;
 }
